95063db3b09f05ce59f9a007b56b48c9c1063819
IREMY250 <ibyishakajeanremy0@gmail.com>
2024-06-20 20:26:25 +0200

    modified

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,8 +1,8 @@
-number1=10;
-number2=5;
-results_addition= int(number1+number2);
-results_subtraction=int(number1-number2);
-results_Multiplication=int(number1*number2);
-print(f"Addition of {number1} and {number2} is {results_addition}");
-print(f"subtraction of {number1} and {number2} is {results_subtraction}");
-print(f"Multiplication of {number1} and {number2} is {results_Multiplication}");
+number1 = 10
+number2 = 5
+results_addition= number1 + number2
+results_subtraction= number1 - number2
+results_Multiplication= number1 * number2
+print(f"Addition of {number1} and {number2} is {results_addition}")
+print(f"subtraction of {number1} and {number2} is {results_subtraction}")
+print(f"Multiplication of {number1} and {number2} is {results_Multiplication}")
