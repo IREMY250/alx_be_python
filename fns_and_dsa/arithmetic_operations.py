6e119c1ca7117931af3d239b410a81a481e790ef
IREMY250 <ibyishakajeanremy0@gmail.com>
2024-07-07 00:57:49 +0200

    arithmetic operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,4 @@
-def perform_operation (num1 : float , num2 : float , operation: str) :
+def perform_operation(num1 : float , num2 : float , operation: str):
     if operation == 'add' :
         return num1 + num2
     elif operation == 'subtract' :
@@ -9,14 +9,14 @@ def perform_operation (num1 : float , num2 : float , operation: str) :
         if num2 == 0 :
             print ("can't divide by zero")
         elif num2 != 0 :
-            return num1 / num2 
+            return num1 / num2
 from arithmetic_operations import perform_operation
 def main():
     print("Arithmetic Operations")
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
     operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
-    result = perform_operation (num1, num2, operation) 
+    result = perform_operation(num1, num2, operation) 
     print(f"Result: {result}")
 
 if __name__ == "__main__" :
