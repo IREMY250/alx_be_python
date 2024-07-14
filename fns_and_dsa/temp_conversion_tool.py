33145918a704faa20271dcc3cfec34c203b0558f
IREMY250 <ibyishakajeanremy0@gmail.com>
2024-07-15 00:10:15 +0200

    bank account

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -8,7 +8,7 @@ def convert_to_fahrenheit(celsius):
     fahrenheit=(celsius*CELSIUS_TO_FAHRENHEIT_FACTOR)+32
     return fahrenheit
 def main():
-    #try:
+    try:
         temp_input = input("Enter the temperature to convert:")
         temperature = float (temp_input)
         unit = input ("Is this temperature in Celsius or Fahrenheit? (C/F): ")
@@ -20,7 +20,7 @@ def main():
             print (f"{temperature} C is {converted_temp} F")
         else:
             print("Invalid unit. Please enter 'C' for Celsius or 'F' for Fahrenheit.")
-    #except ValueError:
-       # print("Invalid temperature. Please enter a numeric value.")
+    except ValueError:
+        print("Invalid temperature. Please enter a numeric value.")
 if __name__ == "__main__":
     main()
---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,14 @@
+class BankAccount :
+    def __init__(self,initial_balance=0): 
+        self.account_balance = initial_balance
+    def deposit(self,amount):
+        self.account_balance+=amount
+        return self.account_balance
+    def withdraw(self,amount):
+        if  self.account_balance > amount : 
+            self.account_balance-=amount
+            return True
+        else :
+            return False
+    def  display_balance():
+         print(f"Current balance: ${self.__account_balance:.2f}")
