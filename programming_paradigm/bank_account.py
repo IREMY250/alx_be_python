1a42305085bc4a99c7fe6ede289ce0cf9f053130
IREMY250 <ibyishakajeanremy0@gmail.com>
2024-07-15 15:21:33 +0200

    bank account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -2,16 +2,13 @@ class BankAccount :
     def __init__(self,initial_balance=0): 
         self.account_balance = initial_balance
     def deposit(self,amount):
-        self.account_balance+=amount
+        self.account_balance = self.account_balance + amount
         return self.account_balance
     def withdraw(self,amount):
-        if  self.account_balance > amount : 
-            self.account_balance-=amount
+        if 0<amount<=self.account_balance: 
+            self.account_balance = self.account_balance - amount
             return True
         else :
             return False
     def  display_balance(self):
          print(f"Current balance: ${self.__account_balance:.2f}")
-# account = BankAccount()
-# account.deposit(500)
-# account.display_balance()
